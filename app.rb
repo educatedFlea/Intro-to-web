a4dfbe168b886e8221f1f3afbdf712f04594e38c
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-02 16:31:24 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "Shush" 
+end 
+
+get '/cat' do 
+  "<div style='border: 5px dashed pink'>
+  <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'
+  </div>"
+end 
