85d32c9fd6d4f3329619f99579aa370af2d16304
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-03 12:32:07 +0000

    intro_params: added params :name

--- app.rb
+++ app.rb
@@ -6,15 +6,22 @@ get '/' do
 end
 
 get '/secret' do
-  "Shush" 
+  "Meow" 
 end 
 
 
-get '/cat' do 
+get '/random-cat' do 
   # the instance variable @name can only be accessed by this route
-  @name = ['Chloe', 'Olivia', 'Ariane'].sample 
+  @name = ['Fluffy fluff', 'Furry ball', 'Hooman extinguisher'].sample 
   # 'ebr' method reads the input file 
   # then process ruby into HTML 
   # the html then return to the 'get' method
+  erb :index
+end 
+
+get '/named-cat' do
+  # what is the p doing here?
+  p params
+  @name = params[:name]
   erb :index
 end 
