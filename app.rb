58290746d37ac13ba79284da18f4daae696094b7
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-03 12:01:15 +0000

    #keeping_views_clean challenge finished

--- app.rb
+++ app.rb
@@ -11,5 +11,10 @@ end
 
 
 get '/cat' do 
-  erb(:index)
+  # the instance variable @name can only be accessed by this route
+  @name = ['Chloe', 'Olivia', 'Ariane'].sample 
+  # 'ebr' method reads the input file 
+  # then process ruby into HTML 
+  # the html then return to the 'get' method
+  erb :index
 end 
