a20a5bad0866ae7c5c7edaef53ea2a26a96f12e4
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-03 14:59:37 +0000

    #posted_params added : post the form rather than get

--- app.rb
+++ app.rb
@@ -19,9 +19,13 @@ get '/random-cat' do
   erb :index
 end 
 
-get '/named-cat' do
+post '/named-cat' do
   # what is the p doing here?
   p params
   @name = params[:name]
   erb :index
+end 
+
+get '/cat-form' do
+  erb :cat_form
 end 
