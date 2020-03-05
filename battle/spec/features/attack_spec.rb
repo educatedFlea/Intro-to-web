f1d87ae677fcb9281e3a8435d28275cdf229dea8
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-05 16:56:14 +0000

    #$gamecreated to replace the global player variable

--- battle/spec/features/attack_spec.rb
+++ battle/spec/features/attack_spec.rb
@@ -8,7 +8,7 @@ feature 'Attacking' do
   scenario 'reduce opponent hit point by 10 after attack' do
     sign_in_and_play
     click_button 'Attack'
-    # click_link 'OK'
+    click_link 'OK'
     expect(page).to have_no_content 'Luigi: 60HP'
     expect(page).to have_content 'Luigi: 50HP'
   end 
--- battle/spec/features/web_helpers.rb
+++ battle/spec/features/web_helpers.rb
@@ -1,5 +1,6 @@
 def sign_in_and_play
   visit('/')
+  @game = Game.new('Mario','Luigi')
   fill_in :player_1_name, with: "Mario"
   fill_in :player_2_name, with: "Luigi"
   click_button "Submit"
